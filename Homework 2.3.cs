612b9251c623eac7ec32701df9cd199061b373fd
AlexanderVerteba <Sashaverteba@gmail.com>
2016-07-17 02:30:30 +0300

    Add files via upload

---
+++ Homework 2.3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5, y = 12, z = 2;
+            int result =-1 << z++;
+            /* 
+             * 5 - 11 + 5=-1
+             * -1>>2=
+             */
+            Console.WriteLine(result);
+        }
+    }
+}
