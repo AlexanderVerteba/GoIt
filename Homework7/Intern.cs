7507012dfd36011189a8c70fa09f02cf88be7fe4
AlexanderVerteba <Sashaverteba@gmail.com>
2016-08-08 05:11:57 +0300

    Add files via upload

---
+++ Homework7/Intern.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework7
+{
+    sealed class Intern:Worker
+    {
+        /* 
+         * Не зрозумів, як потрібно реалізувати даний клас.
+        */
+        
+
+        public override Decimal GetSalary()
+        {
+           return 0;
+        }
+
+        public override void ExtraHours(Int32 _iExtraHoursArg)
+        {
+            
+        }
+    }
+}
