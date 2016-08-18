3fd4399b09d158050670aca439bb004d806933ea
AlexanderVerteba <Sashaverteba@gmail.com>
2016-08-18 04:36:16 +0300

    Add files via upload

---
+++ Homework10/Operations.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework10
+{
+    class Operations
+    {
+        public static void Plus(Int64 _iFirst, Int64 _iSecond)
+        {
+            Console.WriteLine("Result (+): {0}", _iFirst + _iSecond);
+        }
+
+        public static void Minus(Int64 _iFirst, Int64 _iSecond)
+        {
+            Console.WriteLine("Result (-): {0}", _iFirst - _iSecond);
+        }
+
+        public static void Multiply(Int64 _iFirst, Int64 _iSecond)
+        {
+            Console.WriteLine("Result (*): {0}", _iFirst * _iSecond);
+        }
+
+        public static void Divide(Int64 _iFirst, Int64 _iSecond)
+        {
+            if(_iSecond != 0) Console.WriteLine("Result (/): {0}", _iFirst / _iSecond);
+            else Console.WriteLine("Ops! Second operand = 0.");
+        }
+    }
+}
---
+++ Homework10/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework10
+{
+    delegate void MyDelegate(Int64 a, Int64 b);
+
+    class Program
+    {
+        static public event MyDelegate result;
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first operand: ");
+            String _sOper1 = Console.ReadLine();
+            Console.Write("Enter second operand: ");
+            String _sOper2 = Console.ReadLine();
+
+            Console.WriteLine("1: + \n2: - \n3: * \n4: /");
+            String _sOps = Console.ReadLine();
+
+            Int64 fOper = Convert.ToInt64(_sOper1);
+            Int64 sOper = Convert.ToInt64(_sOper2);
+
+            for (int i = 0; i < _sOps.Length; ++i)
+            {
+                if (_sOps[i] == '1')
+                    result += Operations.Plus;
+                else if (_sOps[i] == '2')
+                    result += Operations.Minus;
+                else if (_sOps[i] == '3')
+                    result += Operations.Multiply;
+                else if (_sOps[i] == '4')
+                    result += Operations.Divide;
+            }
+            result.Invoke(fOper, sOper);
+
+        }
+    }
+}
