aa57d59c8a319e743c7067797bea45e1c485c045
AlexanderVerteba <Sashaverteba@gmail.com>
2016-09-05 01:06:13 +0300

    Add files via upload

---
+++ Homework12/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Repository.PupilsRepository.Add(new Pupil() { Name = "Vasya", Age = 10 });
+            Console.WriteLine("Do you want to load information?(1 - yes, 0 - no)");
+            string q = Console.ReadLine();
+            if(q=="1") Repository.Out();
+            Menu();
+        }
+
+        static void Menu()
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("1. Add pupil\n2. Add mark\n3. Get average mark\n4. Show all marks\n5. Save\n6. Exit");
+                int input;
+                int.TryParse(Console.ReadLine(), out input);
+
+                Console.Clear();
+                if (input == 1)
+                {
+                    Repository.AddPupil();                  
+                }
+
+                else if (input == 2)
+                {
+                    Repository.AddMark();
+                }
+
+                else if (input == 3)
+                {
+                    Repository.GetAverageMark();
+                    Console.ReadKey();
+                }
+
+                else if (input == 4)
+                {
+                    Repository.GetMarks();
+                    Console.ReadKey();
+                }
+                else if (input == 5)
+                {
+                    Repository.In();
+                    Console.ReadKey();
+                }
+                else if (input == 6)
+                {
+                    return;
+                }
+                else Console.WriteLine("Only 1-6!\n");
+            }
+        }
+    }
+}
---
+++ Homework12/Repository.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Homework12
+{
+    static class Repository
+    {
+        static  public List<Pupil> PupilsRepository=new List<Pupil>();
+
+        static public void AddPupil()
+        {
+            Console.Write("Name: ");
+            String NameArg = Console.ReadLine();
+            Int32 AgeArg;
+            Console.Write("Age: ");
+            Int32.TryParse(Console.ReadLine(), out AgeArg);
+            PupilsRepository.Add(new Pupil() { Name = NameArg, Age = AgeArg });
+        }
+
+        static public void AddMark()
+        {
+            Int32 Mark;
+            String Subject;
+            Int32 input;
+            bool flag = true;
+            for (int i = 0; i < PupilsRepository.Count; i++)
+            {
+                Console.WriteLine("{0}. {1}", i + 1, PupilsRepository[i].Name);
+            }
+
+            Console.Write("Choose a pupil: ");
+            int.TryParse(Console.ReadLine(), out input);
+            while (flag)
+            {
+                Console.Write("Subject: ");
+                Subject = Console.ReadLine();
+
+                Console.Write("Mark: ");
+                int.TryParse(Console.ReadLine(), out Mark);
+
+                PupilsRepository[input - 1].marks.Add(Subject, Mark);
+
+                Console.Write("Do you want to add one more?(1-yes, 0-no) ");
+                Boolean.TryParse(Console.ReadLine(), out flag);
+            }
+        }
+
+        static public void GetAverageMark()
+        {
+            Int32 input;
+            for (int i = 0; i < PupilsRepository.Count; i++)
+            {
+                Console.WriteLine("{0}. {1}", i + 1, PupilsRepository[i].Name);
+            }
+
+            Console.Write("Choose a pupil: ");
+            int.TryParse(Console.ReadLine(), out input);
+            Console.WriteLine(PupilsRepository[input-1].Average());
+        }
+
+        static public void GetMarks()
+        {
+            Int32 input;
+
+            Console.Write("Enter age: ");
+            int.TryParse(Console.ReadLine(), out input);
+
+            foreach (var item in PupilsRepository)
+            {
+                Console.WriteLine("{0} {1}", item.Name, item.Age);
+                foreach (var subject in item.marks)
+                {
+                    if (item.Age >= input) Console.WriteLine("{0} {1}", subject.Key, subject.Value);
+                }
+                Console.WriteLine(new string('-', 20));
+            }
+
+        }
+
+        static public void In()
+        {
+            FileStream file = new FileStream("C:\\GoIt\\output.txt", FileMode.Truncate, FileAccess.ReadWrite);
+            StreamWriter writer = new StreamWriter(file);
+            writer.WriteLine(PupilsRepository.Count);
+            foreach (var item in PupilsRepository)
+            {
+                writer.WriteLine("{0} {1} {2}", item.Name, item.Age, item.marks.Count);
+                foreach (var mark in item.marks)
+                {
+                    writer.WriteLine("{0} {1}", mark.Key, mark.Value);
+                }
+            }
+            writer.Close();
+        }
+
+        static public void Out()
+        {
+            FileStream file = new FileStream("C:\\GoIt\\output.txt", FileMode.Open, FileAccess.ReadWrite);
+            StreamReader reader = new StreamReader(file);
+            int n;
+            string NameArg;
+            int AgeArg;
+            string SubjectArg;
+            int markArg;
+            int k;
+            Int32.TryParse(reader.ReadLine().ToString(), out n);
+            for (int i = 0; i < n; i++)
+            {
+                NameArg = reader.Read().ToString();
+                AgeArg = reader.Read();
+                k = reader.Read();
+                reader.ReadLine();
+                PupilsRepository.Add(new Pupil() { Name = NameArg, Age = AgeArg });
+                for (int j = 0; j < k; j++)
+                {
+                    SubjectArg = reader.Read().ToString();
+                    markArg = reader.Read();
+                    reader.ReadLine();
+                    PupilsRepository[i].marks.Add(SubjectArg, markArg);
+                }
+            }
+
+            reader.Close();
+            Console.WriteLine("successfully saved!");
+        }
+    }
+}
